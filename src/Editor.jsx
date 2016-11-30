c7d9302e85c100610253c30016a913c4a377e46e
Charles Hsieh <charles@codev.tw>
2016-11-30 21:06:49 +0800

    add state, give init value

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 export default class Editor extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: 'Charles',
+      age: 35,
+      note: 'working at codev_',
+    }
+  }
   render () {
     return (
       <div>
-        name: <input value="Charles" /> <br />
-        age: <input type="number" /> <br />
-        note: <textarea />
+        name: <input value={this.state.name} /> <br />
+        age: <input type="number" value={this.state.age} /> <br />
+        note: <textarea value={this.state.note} />
       </div>
     )
   }
