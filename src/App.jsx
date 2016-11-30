50b521982bb09826d214835e342183d0f3745815
Charles Hsieh <charles@codev.tw>
2016-11-30 20:53:28 +0800

    add Editor Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 
+function Editor () {
+  return (
+    <div>
+      name: <input value="Charles" /> <br />
+      age: <input type="number" /> <br />
+      note: <textarea />
+    </div>
+  )
+}
+
 export default function App () {
   return (
     <div>
       <h1>Recompose Introduction</h1>
       <h3>by Hsieh, Chang-Hsi @codev_</h3>
-      name: <input value="Charles" /> <br />
-      age: <input type="number" /> <br />
-      note: <textarea />
+      <Editor />
     </div>
   )
 }
