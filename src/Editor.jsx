86304a228e963e6367016d73e1130366aea1290e
Charles Hsieh <charles@codev.tw>
2016-11-30 20:58:16 +0800

    convert Editor to a stateful Component

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-export default function Editor () {
-  return (
-    <div>
-      name: <input value="Charles" /> <br />
-      age: <input type="number" /> <br />
-      note: <textarea />
-    </div>
-  )
+export default class Editor extends React.Component {
+  render () {
+    return (
+      <div>
+        name: <input value="Charles" /> <br />
+        age: <input type="number" /> <br />
+        note: <textarea />
+      </div>
+    )
+  }
 }
