d961d346248802239fcf2cc904e5512a469356cb
Charles Hsieh <charles@codev.tw>
2016-11-30 21:13:43 +0800

    add onChange to update state

--- src/Editor.jsx
+++ src/Editor.jsx
@@ -12,9 +12,22 @@ export default class Editor extends React.Component {
   render () {
     return (
       <div>
-        name: <input value={this.state.name} /> <br />
-        age: <input type="number" value={this.state.age} /> <br />
-        note: <textarea value={this.state.note} />
+        name:
+        <input
+          value={this.state.name}
+          onChange={event => this.setState({ name: event.target.value })}
+        /> <br />
+        age:
+        <input
+          type="number"
+          value={this.state.age}
+          onChange={event => this.setState({ age: event.target.value })}
+        /> <br />
+        note:
+        <textarea
+          value={this.state.note}
+          onChange={event => this.setState({ note: event.target.value })}
+        />
       </div>
     )
   }
