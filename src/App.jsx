1b003dbb0d96a72495208f47b46be51ac14dcf80
Charles Hsieh <charles@codev.tw>
2016-11-30 20:51:50 +0800

    add inputs

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ export default function App () {
     <div>
       <h1>Recompose Introduction</h1>
       <h3>by Hsieh, Chang-Hsi @codev_</h3>
+      name: <input value="Charles" /> <br />
+      age: <input type="number" /> <br />
+      note: <textarea />
     </div>
   )
 }
